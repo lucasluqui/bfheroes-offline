310117f8281d69f3a808763ef41c62ba931ebcf8
Lucas Allegri <lucasallegri@protonmail.com>
2019-04-12 03:47:55 -0300

    removed chars arg from random_str function

--- src/util/StringUtil.py
+++ src/util/StringUtil.py
@@ -1,5 +1,6 @@
 import string
 import random
 
-def random_str(size, chars=string.ascii_uppercase+string.digits+string.ascii_lowercase):
-    return ''.join(random.choice(chars) for _ in range(size))
+def random_str(size):
+    CHARS = string.ascii_uppercase+string.digits+string.ascii_lowercase
+    return ''.join(random.choice(CHARS) for _ in range(size))
