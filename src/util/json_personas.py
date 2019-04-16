1638babc68a454cc4ddf56dfe8bc657a9ea345eb
Lucas Allegri <lucasallegri@protonmail.com>
2019-04-16 20:23:27 -0300

    Fixed an error in path.

--- src/util/json_personas.py
+++ src/util/json_personas.py
@@ -2,7 +2,7 @@ import json
 from configobj import ConfigObj
 
 CONFIG = ConfigObj('src/config.ini')
-path = CONFIG['Settings']['HeroesPath']
+path = CONFIG['Paths']['HeroesPath']
 
 with open(path+'globalStats.json') as global_stats:
     global_stats = json.load(global_stats)
